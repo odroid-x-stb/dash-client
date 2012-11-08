72742cf56e986d57d3d9291a9c7ab6461f2255e7
Fabien Fleurey <fabien@fleurey.org>
2012-11-08 14:39:27 +0100

    Add maven archetype

---
+++ dashclient-main/src/main/java/odroidx/enseirb/fr/MainActivity.java
@@ -0,0 +1,14 @@
+package odroidx.enseirb.fr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainactivity_layout);
+	}
+	
+}
