ede7a46e4afb9fbf51455d084393824e2fa58275
Fabien Fleurey <fabien@fleurey.org>
2012-11-29 20:17:59 +0100

    Test a native call

--- dashclient-main/src/main/java/fr/enseirb/odroidx/MainActivity.java
+++ dashclient-main/src/main/java/fr/enseirb/odroidx/MainActivity.java
@@ -2,6 +2,7 @@ package fr.enseirb.odroidx;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +10,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mainactivity_layout);
+		DashManager manager = new DashManager();
+		int ret = manager.openStream("http://testaddress.fr");
+		Log.e("DEBUG", String.valueOf(ret));
 	}
 	
 }
